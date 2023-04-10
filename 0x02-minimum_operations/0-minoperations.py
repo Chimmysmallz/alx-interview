ed407a129fad3c42d7f95eff4759649c166ea7f9
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-04-10 11:45:03 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+def minOperations(n):
+    if n % 2 == 1:
+        return 0
+    count = 0
+    h_count = 2
+    while h_count < n:
+        if n % h_count == 0:
+            count += 2  # copy all and paste
+            h_count *= 2
+        else:
+            count += 1  # paste
+            h_count += h_count
+    if h_count == n:
+        count += 2  # copy all and paste
+    return count
