2f43dd41a12a819b3724e9c041a502086a673f33
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-04-14 06:04:58 +0100

    Create 0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,32 @@
+import sys
+from collections import defaultdict
+
+total_size = 0
+status_codes = defaultdict(int)
+line_count = 0
+
+try:
+    for line in sys.stdin:
+        line_count += 1
+        parts = line.split()
+        if len(parts) != 7:
+            continue
+        ip, _, _, _, status, size, _ = parts
+        if not status.isdigit():
+            continue
+        status = int(status)
+        if status not in status_codes:
+            status_codes[status] = 0
+        status_codes[status] += 1
+        total_size += int(size)
+        if line_count % 10 == 0:
+            print(f"Total file size: {total_size}")
+            for code in sorted(status_codes.keys()):
+                print(f"{code}: {status_codes[code]}")
+            print()
+except KeyboardInterrupt:
+    print("Interrupted by user")
+
+print(f"Total file size: {total_size}")
+for code in sorted(status_codes.keys()):
+    print(f"{code}: {status_codes[code]}")
