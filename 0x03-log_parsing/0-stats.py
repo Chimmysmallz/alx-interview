81fde37efb3be1f73b216a001b1dd895bd8ee670
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-04-16 17:49:55 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
 
+import random
 import sys
-from collections import defaultdict
+from time import sleep
+import datetime
 
-total_size = 0
-status_codes = defaultdict(int)
-line_count = 0
-
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-        if len(parts) != 7:
-            continue
-        ip, _, _, _, status, size, _ = parts
-        if not status.isdigit():
-            continue
-        status = int(status)
-        if status not in status_codes:
-            status_codes[status] = 0
-        status_codes[status] += 1
-        total_size += int(size)
-        if line_count % 10 == 0:
-            print(f"Total file size: {total_size}")
-            for code in sorted(status_codes.keys()):
-                print(f"{code}: {status_codes[code]}")
-            print()
-except KeyboardInterrupt:
-    print("Interrupted by user")
-
-print(f"Total file size: {total_size}")
-for code in sorted(status_codes.keys()):
-    print(f"{code}: {status_codes[code]}")
+for i in range(10000):
+    #sleep(random.random())
+    #sleep(0.05)
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
