c068f1f7b84905c0160e0553aa9cfba5060899ab
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-05-12 16:02:01 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,29 +9,43 @@ base_url = "https://swapi-api.alx-tools.com/api/"
 if len(sys.argv) > 1:
     movie_id = sys.argv[1]
     movie_url = base_url + movie_id + "/"
-    response = requests.get(movie_url)
-    if response.status_code == 404:
-        print("Movie not found")
+    try:
+        response = requests.get(movie_url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
     else:
         movie_data = response.json()
         character_urls = movie_data["characters"]
         for character_url in character_urls:
-            response = requests.get(character_url)
-            character_data = response.json()
-            print(character_data["name"])
+            try:
+                response = requests.get(character_url)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred: {e}")
+            else:
+                character_data = response.json()
+                print(character_data["name"])
 # If no movie ID is provided, print the characters for all movies
 else:
     for i in range(1, 8):
         movie_url = base_url + str(i) + "/"
-        response = requests.get(movie_url)
-        if response.status_code == 404:
-            print(f"Movie ID {i} not found")
+        try:
+            response = requests.get(movie_url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
         else:
             movie_data = response.json()
             print(f"Characters for {movie_data['title']}:")
             character_urls = movie_data["characters"]
             for character_url in character_urls:
-                response = requests.get(character_url)
-                character_data = response.json()
-                print(character_data["name"])
+                try:
+                    response = requests.get(character_url)
+                    response.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    print(f"An error occurred: {e}")
+                else:
+                    character_data = response.json()
+                    print(character_data["name"])
             print()
