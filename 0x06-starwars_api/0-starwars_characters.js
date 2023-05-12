6e1a09075a4cbba6b1fa28101be2d788873a4b91
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-05-12 16:04:11 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,48 @@
 #!/usr/bin/node
 
-import requests
-import sys
+const requests = require('requests');
+const sys = require('sys');
 
-base_url = "https://swapi-api.alx-tools.com/api/"
+const base_url = "https://swapi-api.alx-tools.com/api/films/";
 
-# If a movie ID is provided as an argument, print its characters
-if len(sys.argv) > 1:
-    movie_id = sys.argv[1]
-    movie_url = base_url + movie_id + "/"
-    try:
-        response = requests.get(movie_url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-    else:
-        movie_data = response.json()
-        character_urls = movie_data["characters"]
-        for character_url in character_urls:
-            try:
-                response = requests.get(character_url)
-                response.raise_for_status()
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred: {e}")
-            else:
-                character_data = response.json()
-                print(character_data["name"])
-# If no movie ID is provided, print the characters for all movies
-else:
-    for i in range(1, 8):
-        movie_url = base_url + str(i) + "/"
-        try:
-            response = requests.get(movie_url)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-        else:
-            movie_data = response.json()
-            print(f"Characters for {movie_data['title']}:")
-            character_urls = movie_data["characters"]
-            for character_url in character_urls:
-                try:
-                    response = requests.get(character_url)
-                    response.raise_for_status()
-                except requests.exceptions.RequestException as e:
-                    print(f"An error occurred: {e}")
-                else:
-                    character_data = response.json()
-                    print(character_data["name"])
-            print()
+// If a movie ID is provided as an argument, print its characters
+if (sys.argv.length > 2) {
+    const movie_id = sys.argv[2];
+    const movie_url = base_url + movie_id + "/";
+    requests.get(movie_url, (err, response, body) => {
+        if (response.statusCode === 404) {
+            console.log("Movie not found");
+        } else {
+            const movie_data = JSON.parse(body);
+            const character_urls = movie_data["characters"];
+            character_urls.forEach(character_url => {
+                requests.get(character_url, (err, response, body) => {
+                    const character_data = JSON.parse(body);
+                    console.log(character_data["name"]);
+                });
+            });
+        }
+    });
+}
+// If no movie ID is provided, print the characters for all movies
+else {
+    for (let i = 1; i <= 7; i++) {
+        const movie_url = base_url + i + "/";
+        requests.get(movie_url, (err, response, body) => {
+            if (response.statusCode === 404) {
+                console.log(`Movie ID ${i} not found`);
+            } else {
+                const movie_data = JSON.parse(body);
+                console.log(`Characters for ${movie_data['title']}:`);
+                const character_urls = movie_data["characters"];
+                character_urls.forEach(character_url => {
+                    requests.get(character_url, (err, response, body) => {
+                        const character_data = JSON.parse(body);
+                        console.log(character_data["name"]);
+                    });
+                });
+                console.log();
+            }
+        });
+    }
+}
