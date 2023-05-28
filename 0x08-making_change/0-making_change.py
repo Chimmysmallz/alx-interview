50b4762a997414d522b923be1359d652ac42ff11
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-05-28 15:33:02 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 def makeChange(coins, total):
-    if total < 0:
+    if total <= 0:
         return 0
 
-    # Create a list to store the minimum number of coins needed for each value
+    # Create a list to store the fewest number of coins needed for each total amount
     min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0
 
-    # Iterate through each coin value
-    for coin in coins:
-        # Calculate the minimum number of coins needed for each value up to the total
-        for value in range(coin, total + 1):
-            min_coins[value] = min(min_coins[value], min_coins[value - coin] + 1)
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
 
     if min_coins[total] == float('inf'):
         return -1
@@ -20,6 +19,6 @@ def makeChange(coins, total):
     return min_coins[total]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(makeChange([1, 2, 25], 37))
     print(makeChange([1256, 54, 48, 16, 102], 1453))
