c81573512d54caf3ea7c29c57a3bcd1ef5d61dee
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-05-28 15:27:44 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,48 +1,29 @@
 #!/usr/bin/node
 
-const requests = require('requests');
-const sys = require('sys');
+const request = require('request');
 
-const base_url = "https://swapi-api.alx-tools.com/api/films/";
+const movieId = process.argv[2];
 
-// If a movie ID is provided as an argument, print its characters
-if (sys.argv.length > 2) {
-    const movie_id = sys.argv[2];
-    const movie_url = base_url + movie_id + "/";
-    requests.get(movie_url, (err, response, body) => {
-        if (response.statusCode === 404) {
-            console.log("Movie not found");
-        } else {
-            const movie_data = JSON.parse(body);
-            const character_urls = movie_data["characters"];
-            character_urls.forEach(character_url => {
-                requests.get(character_url, (err, response, body) => {
-                    const character_data = JSON.parse(body);
-                    console.log(character_data["name"]);
-                });
-            });
-        }
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characterUrls = movie.characters;
+
+  characterUrls.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
-}
-// If no movie ID is provided, print the characters for all movies
-else {
-    for (let i = 1; i <= 7; i++) {
-        const movie_url = base_url + i + "/";
-        requests.get(movie_url, (err, response, body) => {
-            if (response.statusCode === 404) {
-                console.log(`Movie ID ${i} not found`);
-            } else {
-                const movie_data = JSON.parse(body);
-                console.log(`Characters for ${movie_data['title']}:`);
-                const character_urls = movie_data["characters"];
-                character_urls.forEach(character_url => {
-                    requests.get(character_url, (err, response, body) => {
-                        const character_data = JSON.parse(body);
-                        console.log(character_data["name"]);
-                    });
-                });
-                console.log();
-            }
-        });
-    }
-}
+  });
+});
